aafbcc0c9d2f006226df13a2534658af816c0d28
alan <35761977+thealanle@users.noreply.github.com>
2019-10-27 12:28:25 -0700

    Begin accessing NCBI Blast through Biopython

---
+++ concept.py
@@ -0,0 +1,23 @@
+import requests
+from Bio.Blast import NCBIWWW, NCBIXML
+
+INPUT_FILE = 'input_files/hbb.fasta'
+BLAST_OUTPUT = 'output_files/my_blast.xml'
+#
+# with open('input_files/hbb.fasta', 'r') as f:
+#     title = ''
+#     sequence = ''
+#     for line in f.readlines():
+#         if line[0] == '>':
+#             title = line.strip()
+#         else:
+#             sequence += line.strip()
+
+with open(INPUT_FILE, 'r') as f:
+    fasta_string = f.read()
+print(fasta_string)
+
+result_handle = NCBIWWW.qblast("blastn", "nt", fasta_string)
+
+with open(BLAST_OUTPUT, "w") as f_out:
+    f_out.write(result_handle.read())
