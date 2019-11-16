318e83befc520d762075d731132227bf3de67d7b
alan <35761977+thealanle@users.noreply.github.com>
2019-11-15 22:37:57 -0800

    Add radio buttons to form

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, RadioField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class QueryForm(FlaskForm):
     query = StringField(label='Query', validators=[DataRequired()])
+    have = RadioField(label='Have:', choices=[
+                      ('dna', 'DNA'), ('mrna', 'mRNA')])
     submit = SubmitField(label='Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.forms import QueryForm
 
 
@@ -9,7 +9,10 @@ def index():
     return render_template('index.html', title='bio-pipelines')
 
 
-@app.route('/query')
+@app.route('/query', methods=['GET', 'POST'])
 def query():
     form = QueryForm()
+    if form.validate_on_submit():
+        flash(f"Submitted the following string: {form.query.data}")
+        return redirect('/index')
     return render_template('query.html', title='Submit a Query', form=form)
