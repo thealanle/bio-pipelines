22f0ecce41207645dec4d8c1f45de17ca8f936f7
alan <35761977+thealanle@users.noreply.github.com>
2019-11-16 17:21:28 -0800

    Rename bio-pipelines.py to bio_pipelines.py

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ from app.forms import QueryForm
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='bio-pipelines')
+    return render_template('index.html', title='bio_pipelines')
 
 
 @app.route('/query', methods=['GET', 'POST'])
