0f9349cf194a0c3073572e23ca4fd628b81b3b6c
alan <35761977+thealanle@users.noreply.github.com>
2019-11-16 21:10:40 -0800

    Add whitespace for easier reading

--- app/forms.py
+++ app/forms.py
@@ -5,9 +5,12 @@ from wtforms.validators import DataRequired
 
 class QueryForm(FlaskForm):
     query = StringField(label='Query:', validators=[DataRequired()])
+
     have = RadioField(label='Have:', choices=[
                       ('dna', 'DNA'), ('mrna', 'mRNA'), ('protein', 'Protein')])
+
     want = RadioField(label='Want:', choices=[
                       ('dna', 'DNA'), ('mrna', 'mRNA'), ('protein', 'Protein'),
                       ('wiki', 'Wikipedia Results'), ('diseases', 'Related Diseases'), ('hits', 'BLAST Hits')])
+
     submit = SubmitField(label='Submit')
