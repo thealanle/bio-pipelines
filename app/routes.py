83b9cb9d8c4640c96daa4b5660e96d8c3c677543
alan <35761977+thealanle@users.noreply.github.com>
2019-11-24 12:10:51 -0800

    Implement WikiSearch into webapp

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template, flash, redirect, url_for, request
 from app.forms import QueryForm
+import bio_pipelines
 
 
 @app.route('/')
@@ -17,19 +18,33 @@ def query():
                 'have': form.have.data, 'want': form.want.data}
         flash(f"Submitted the following string: {data['search_term']}")
         print(data)
-        # return redirect(url_for('result'))
         # return render_template('result.html', form=form, sequence=form.query.data, have=form.have.data, want=form.want.data)
     return render_template('query.html', title='Submit a Query', form=form)
 
 
 @app.route('/result', methods=['POST'])
 def result():
-    form = request.form
+
     if request.method == 'POST':
+        form = {'query': request.form.get('query'),
+                'have': request.form.get('have'),
+                'want': request.form.get('want')}
+
+        results = bio_pipelines.WikiSearch(
+            form['query']).get_hrefs()
+
         print(">>>>>POST request received. Rendering result.html...")
-        return render_template('result.html', title='Results', form=form, sequence=form.get('query'), have=form.get('have'), want=form.get('want'))
+
+        return render_template('result.html', title='Results', query=form['query'], have=form['have'], want=form['want'], results=results)
 
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+def sanitize(query):
+    if query[0] == '>':
+        return query.split('\n')[1:]
+    else:
+        return ''.join(query.split('\n'))
